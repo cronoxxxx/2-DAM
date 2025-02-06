4fd7a999371d18e4236908e84fc32ec491fffab5
Adrian Saavedra <153601542+cronoxxxx@users.noreply.github.com>
2025-02-06 01:47:10 +0100

    6 febrero 2025

--- PSP/Proyectos/LoginSpring_AdrianSaavedra/src/main/java/org/example/loginspring_adriansaavedra/ui/filters/SessionFilter.java
+++ PSP/Proyectos/LoginSpring_AdrianSaavedra/src/main/java/org/example/loginspring_adriansaavedra/ui/filters/SessionFilter.java
@@ -6,27 +6,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.example.loginspring_adriansaavedra.common.Constantes;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
 public class SessionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String path = request.getRequestURI().substring(request.getContextPath().length());
+        HttpSession session = request.getSession(false);
 
-        if (path.startsWith(Constantes.PLAYERS_DIR) || path.startsWith(Constantes.UPDATE_DIR)) {
-            HttpSession session = request.getSession(false);
-
-            if (session == null || session.getAttribute(Constantes.USER) == null) {
-                response.sendRedirect(request.getContextPath() + Constantes.LOGIN_DIR);
-                return;
-            }
+        if (session == null || session.getAttribute(Constantes.USER) == null) {
+            response.sendRedirect(request.getContextPath() + Constantes.LOGIN_DIR);
+            return;
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        return !(path.startsWith(Constantes.PLAYERS_DIR) || path.startsWith(Constantes.UPDATE_DIR));
     }
 }
