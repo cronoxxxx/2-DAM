21b3e3900eeef3056f2a34401f62ca0ebb32f7f0
Adrian Saavedra <153601542+cronoxxxx@users.noreply.github.com>
2025-02-18 12:31:27 +0100

    finish third test

--- MOBILES/PlayersApp_AdrianSaavedra/app/build.gradle.kts
+++ MOBILES/PlayersApp_AdrianSaavedra/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "API_URL", "\"http://192.168.1.65:8080\"")
+        buildConfigField("String", "API_URL", "\"http://192.168.3.143:8080\"")
         vectorDrawables {
             useSupportLibrary = true
         }
